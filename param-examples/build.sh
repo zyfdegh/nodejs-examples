ba82e4c4d1364038d5b3801ecbd2eadd6ce1bf41
zyfdegh <zyfdegg@gmail.com>
2017-03-05 09:53:44 +0800

    add Dockerfile

---
+++ param-examples/build.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+sudo docker build -t zyfdedh/nodejs-param-example:dev .
+
+# Run
+# sudo docker run -d -p 3000:3000 zyfdedh/nodejs-param-example:dev
+
+# Stop
+# sudo docker kill <ContainerID>
--- router-examples/build.sh
+++ router-examples/build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+## Build manually on CentOS
+
 # yum install -y nodejs
 # npm install express
 # mkdir router-example-1 && cd router-example-1
@@ -10,3 +12,14 @@
 # alias jsfmt='js-beautify -r'
 # jsfmt -r index.sh
 # node index.js
+
+## Build with docker
+
+sudo docker build -t zyfdedh/nodejs-router-example:dev .
+
+# Run
+# sudo docker run -d -p 3000:3000 zyfdedh/nodejs-router-example:dev
+
+# Stop
+# sudo docker kill <ContainerID>
+
