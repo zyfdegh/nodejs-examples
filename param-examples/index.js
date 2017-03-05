40334cb35b81db919d6998942e9730bc8947ffc5
zyfdegh <zyfdegg@gmail.com>
2017-03-05 10:18:51 +0800

    parse query param, body

--- param-examples/index.js
+++ param-examples/index.js
@@ -1,5 +1,12 @@
 var express = require('express')
+var bodyParser = require('body-parser')
+
 var app = express()
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+app.use(bodyParser.json())
 
 app.get('/', function(req, resp) {
     resp.send('hello')
@@ -13,7 +20,7 @@ app.get('/users/:id', function(req, resp) {
 
 // this api shows how to convert km to mile, or mile to km
 // convert unit1(value) to unit2
-// 
+//
 // get /convert/km-mile/1
 // get /convert/mile-km/1
 app.get('/convert/:unit1-:unit2/:value', function(req, resp) {
@@ -38,6 +45,29 @@ app.get('/convert/:unit1-:unit2/:value', function(req, resp) {
         obj.result = result
         resp.send(obj)
     }
+})
+
+// query params
+// get /users?limit=10&offset=20
+app.get("/users", function(req, resp) {
+    limit = req.query.limit
+    offset = req.query.offset
+    console.log("limit: " + limit + ", offset: " + offset)
+    resp.send(req.query)
+})
+
+// decode(if need) body
+// post /users
+// {
+//     "username": "tom",
+//     "password": "secret"
+// }
+app.post("/users", function(req, resp) {
+    username = req.body.username
+    password = req.body.password
+    console.log(username)
+    // console.log(password)
+    resp.send(req.body)
 })
 
 app.listen(3000, function() {
